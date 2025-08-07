49c50fca385761b5b8d411247b9132afcdfecf70
Dharun S <dharunsakthivel720@gmail.com>
2025-08-07 14:23:43 +0530

    demo

---
+++ string.py
@@ -0,0 +1,31 @@
+# print("python is fun") #Basic print statement
+
+# print("Hello World") #Print statement with string
+
+# print('Hello World') #Print statement with single quotes
+
+# a="dharun" #String variable
+# print(a) #Print string variable
+
+# a='''cricket is 
+# the best game
+# is emotion'''
+# print(a) #Multiline string
+
+# a="dharun in loic"
+# print(a [1:3]) #Print string variable with space
+
+# a="dharun in loic"
+# print(a[1:]) #Print string variable from index 1 to end
+
+# a="dharun in loic"
+# print(a[-5:-1] ) #Print string variable from start to index 5
+
+a="developing"
+print(a.upper())    #Convert string to uppercase
+
+a="DHARUN IN ERU"
+print(a.lower())    #Convert string to lowercase
+
+a="dharun was here"
+print(a.replace("d","e"))  #Replace character in string
