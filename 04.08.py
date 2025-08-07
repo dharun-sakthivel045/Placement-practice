c7ad0faec50fbab6b561607ff66eeaaac46beb85
dharun-sakthivel045 <dharunsakthivel720@gmail.com>
2025-08-07 12:42:56 +0530

    4th August 2025 work

---
+++ 04.08.py
@@ -0,0 +1,25 @@
+a=1  #case senstive
+A=4
+print(a)
+print(A)
+
+Myvar="ece"     #variable name
+myvar="ece"
+my_var="ece"
+print(Myvar)
+print(myvar)
+print(my_var)
+
+x,y,z=("dharun", "sanjay", "sakthivelgomathi") #assign muliple variable
+print(x)
+print(y)
+print(z)    
+
+x=y=z="eru"  #one value to multiple variable
+print(x)
+print(y)
+print(z)
+
+def add(a:int,b:int): # global variable
+    return a+b
+print(add(3,1000))    
---
+++ pratcise.py
@@ -0,0 +1,388 @@
+# # name=input()
+# # age=input()
+# # address=input()
+# # print("My name is:",name)
+# # print("My age is:",age)
+# # print("My adress is:",address)
+# # import sys
+# # print(sys.version)
+# # if 5>2:
+# #      print ("five is greater than two")
+# # if 5>2:
+# #     print ("five is than grater than two")
+# # x=7
+# # y=("hello world")
+# # print(x)
+# # print(y)
+# # a=19
+# # b=("Dharun")
+# # print(a)
+# # print(b)
+# # a=int(3)                              #variable casting
+# # b=str(4)
+# # c=float(6)
+# # print(a)
+# # print(b)
+# # print(c)
+# # a=5                                            #get the type
+# # b=("dharun")
+# # print(type(a))
+# # print(type(b))
+# # a=4                          #variables are case sensiteve
+# # A=('dharun')
+# # print(a)
+# # print(A)
+# # mYvar=('dharun')          #variables name  
+# # Myvar=('dharun')
+# # myvar1=('dharun')
+# # print(mYvar)
+# # print(Myvar)
+# # print(myvar1)
+# # x, y, z, ="orange","apple","lime"#assine value to multiple variable
+# # print(x)
+# # print(y)
+# # print(z)
+# # x = y = z = "Orange"
+# # print(x)
+# # print(y)
+# # print(z)
+# # e=5
+# # e=7
+# # e=8
+# # print(e)
+# # fruit=['apple','banana','pineapple']  #unpack collection
+# # x , y , z ,=fruit
+# # print(x)
+# # print(y)
+# # print(z)
+# # x=('dharun')
+# # print(x)
+
+# # x=('dharun')
+# # y=('is')
+# # print(x,y)
+
+# # x=10
+# # y='jhon'
+# # print(x,y)
+
+# def dharun(banana:str):
+#     return'shjk'+banana
+# print(dharun('nnn'))
+
+# def dharun(apple:str):
+#     return"san"+apple
+# print(dharun("ahhh"))
+
+# def sub(x:int,y:int):
+#     return x-y
+# print(sub(3,1))
+
+# def paninter(paning:str):
+#     return "wall"+paning
+# print(paninter("blue"))
+
+# def ece(student:str):
+#     return "topper"+student
+# print(ece("call"))
+
+# def mul(x:int,y:int):
+#     return x*y
+# print(mul(3,7,))
+
+
+# def calculatation(x:int,y:int):
+#     return x+y
+# print(calculatation(4,8888))
+
+# x=y=z="erut"
+# print(x)
+# print(y)
+# print(z)
+
+# x,y,z,=("eru","cat","dog")
+# print(x)
+# print(y)
+# print(z)
+
+# x=5 # int type
+# print(x)
+# print(type(x)) 
+
+# x="dharun" #str type
+# print(x)
+# print(type(x))
+
+# x=("dharun","eru","mar")  #tuple type
+# print(x)
+# print(type(x))
+
+# x=["dharun","eru","mar"]  #list type
+# print(x)
+# print(type(x))
+
+# x=20.5  #float type
+# print(x)
+# print(type(x))
+
+# x={"dharun","eru","mar"}      #set type
+# print(x)
+# print(type(x))
+
+# x={"list":1,"num":2}   #dict
+# print(type(x))
+
+# x=True            #bool
+# print(type(x))
+
+# print(5>7)
+# print(5<7)
+
+# x=3+5j    #complex type
+# y=4+7j
+# print(x+y)
+# print(type(x))
+# print(type(y))
+
+# x=5  #changing thje data types
+# y=7.0
+# a=float(x)
+# b=int(y)
+# print(type(a))
+# print(type(b))
+
+
+# x=int(5)
+# y=int("4")
+# z=int(9.0)
+# print(x)
+# print(y)
+# print(z)
+
+# x=int("44")    #casting method
+# y=float(4)
+# print(x)
+# print(y)
+
+
+# print("hi")
+
+# print("hello'world'")
+
+# a='dharun'
+# print(a)
+
+
+# a='''dharun is    #multilinr string
+# good
+# boy
+# and'''
+# print(a)
+
+# a=('hellw world') #indexing
+# print(a[1])
+
+# a=('dharun')
+# print(a[4])
+
+# for x in "dharun":   #loop string
+#     print(x)
+
+# x=("dharun") #string length
+# print(len(x))
+
+# x=("eru is the ba")
+# print("dha" in x)
+
+# e=("love is emotion") #silicing string
+# print(e[2:11])
+    
+# e=("love is emmotion")
+# print(e[3:])
+
+# e=("dharun")
+# print(e[:1])
+
+# a=("dharun")
+# print(a.upper())
+
+# a="college"
+# print(a[0:2].lower()+a[2:4].upper()+a[4:].lower())
+
+# a="sanJay"
+# print(a[0:3].lower()+a[3:4].upper()+a[4:].lower())
+
+# a="47557yhyuhjbhvew"
+# b=[]
+# c=[]
+# for i in a:
+#     if i.isalpha():
+#         b.append(i)
+#     else:
+#         i.isnumeric()
+#         c.append(i)
+# print(b)
+# print(c)
+
+
+# a="dharun going"
+# print(a.capitalize())
+
+# a="dharun was"
+# print(a.title())
+
+# a="dharun"
+# print(a.isnumeric())
+
+# a="94900039"
+# print(a.isalnum())
+
+# a="122wsdd"
+# print(a.isalnum())
+
+# a="dharun"
+# print(a.replace('a','s'))
+
+# a="dharrun"
+# print(a.islower())
+
+# a='20/01/26'
+# print(a.split('o'))
+
+# age=45
+# con=f"my name is dharun, my age is {age}"
+# print(con)
+
+# age=input()
+# print(f"my {age} is age")
+
+# print(4>5)
+# print(4>8)
+
+# a=100
+# b=399
+# if a > b:
+#     print("a is greater than b")
+# else:
+#     print("a is lesser than b")
+    
+
+
+# a=int(input("enter your age"))
+# if a>=18:print("a is edigible for vote")
+# else:print("a is not eligible for vote")
+
+# a="18"
+# # print(type(int(a)))
+
+
+# a=45
+# b=45
+# print(a+b)
+
+# a=95
+# b=44
+# print(a-b)
+
+# a=44
+# b=4
+# print(a*b)
+
+# a=44
+# b=9
+# print(a/b)
+
+# a=45
+# b=33
+# print(a%b)
+
+# a=4
+# y=4
+# print(a ** y)
+
+# a=10
+# b=3
+# print(a/b)
+
+# x=333
+# print(x)
+
+# x=4
+# x +=6
+# print(x)
+
+# g=6
+# g+=9
+# print(g)
+
+
+# h=9
+# h-=5
+# print(h)
+
+# k=3333
+# k*=3
+# print(k)
+
+# d=55
+# d/=4
+# print(d)
+
+# a=10
+# b=3
+# print(a%b)
+
+# list=["dharun","eru","dharun","sanjay"]
+# list.pop()
+# print(list)
+
+# hi="dharun"
+# print(hi.capitalize())
+
+# a='dharun was the'
+# print(a.title())
+
+# a=["dharun","dharun","sakthi","power"]
+# a.append("apple")
+# print(a)
+
+# b=["dharun","sanjay","orange"]
+# print(b.pop(2))
+
+# a=["dharun",44,True,"orange"]
+# print(a.pop(3))
+
+# a=["dharun","orage","carrot"]
+# b=[5,7,9,0]
+# c=[True,False,True]
+# print(a)
+# print(b)
+# print(c)
+
+
+# s = "hello"
+# reversed = s[::-1]
+# print(reversed)
+
+
+# a="dharun"
+# print(a[::-1])
+
+# a="""dharun was     #multiline string
+# eating in the 
+# hotel"""
+# print(a)
+
+# a="dharun"
+# b="eru"
+# print(a+b)
+
+
+a="dharun"
+b=" good boy"
+c=" was"
+print(a + c + b)
+
+
+
+
