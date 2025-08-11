a05dabf819a2ba71c54e8ab3b855d92642f4c8df
Dharun S <dharunsakthivel720@gmail.com>
2025-08-11 19:59:34 +0530

    Add files via upload
    append,pop,remove,count,copy,clear,sort,index,remove

---
+++ List_Methods.py
@@ -0,0 +1,41 @@
+# a=[2,3,7,8]   #append
+# a.append(2)
+# print(a)
+
+# a=[2,4,7,8]   #clear
+# a.clear()
+# print(a)
+
+# a=[2,4,7,8]  #copy
+# b=a.copy()
+# print(b)
+
+# a=[2,4,7,8,2]   #count
+# b=a.count(2)
+# print(b)
+
+# a=[1,8,5,7,3] #sort small to big
+# a.sort()
+# print(a)
+
+# a=[1,8,5,4,8]    #sort high to small
+# a.sort(reverse=True)    
+# print(a)
+
+# a=[1,8,5,4,8]    #remove last element
+# a.pop()
+# print(a)
+
+# a=[1,8,5,4,8]  #insert element
+# a.insert(1,33)
+# print(a)
+
+# a=[1,8,5,4,8] #index
+# b=a.index(9945678)
+# print(b)
+
+# a=[1,5,5,9,54] # it will remove the first occurrence 
+# a.remove(5)
+# print(a)
+
+
