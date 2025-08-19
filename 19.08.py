1c92f0891644e5b7822ed43966e06fcc7235ddeb
Dharun S <dharunsakthivel720@gmail.com>
2025-08-19 20:00:32 +0530

    Add files via upload

---
+++ 19.08.py
@@ -0,0 +1,26 @@
+# a=('dharun','nan','sam')   #if 'dfghj' in a:
+# if 'dfghj' in a:
+#     print("dfghj is there")
+# else:
+#     print("dfghj is not there")
+
+# a=('dharun','nan','sam')
+# b=list(a)  # Converting tuple to list
+# b[1] = 'coder'  # Modifying the list
+# a = tuple(b)  # Converting back to tuple
+# print(a)  
+
+# a=('dharun','coder','sam')
+# b=list(a)  # Converting tuple to list (no change)
+# b.append('new')  
+# a = tuple(b)  
+# print(a)
+
+# a=('dharun','coder','sam')
+# b=('dfgjxxj',)
+# a += b  # Concatenating tuples
+# print(a) 
+
+
+a=('dharun','coder','sam')
+print(a)
