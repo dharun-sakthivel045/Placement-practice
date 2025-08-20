9215ccffe6a5ff0dd791cf7c79f947c341180dc0
Dharun S <dharunsakthivel720@gmail.com>
2025-08-20 19:29:07 +0530

    Add files via upload

---
+++ 20.08.py
@@ -0,0 +1,37 @@
+# name=('dharun','nan','john')   # unpacking collection
+# (ssmkc,wdcdc,cedce)=name
+# print(ssmkc)
+# print(wdcdc)
+# print(cedce)
+
+# vegetables=('carrot','potato','tomato','broccoli','spinach')    # unpacking collection  If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+# (c1,c2,*c3)=vegetables
+# print(c1)
+# print(c2)
+# print(c3)
+
+# name=('dharun','nan','john','sam','tata','mah')
+# (carrot,*apple,beetroot)=name  # unpacking collection with * operator
+# print(carrot)
+# print(apple)
+# print(beetroot)
+
+# x=('carrot','potato','tomato','broccoli','spinach')
+# for i in x:  # Iterating through a tuple
+#     print(i)
+
+# x=('carrot','potato','tomato','broccoli','spinach')  # Iterating through a tuple
+# for i in range(len(x)):
+#     print(x[i])
+
+# a=('dahrun','carrot')   #tuples for adding two variables
+# b=(True,False)
+# c=a+b
+# print(c)
+
+# vegetables=('carrot','potato','tomato','broccoli','spinach')  #multiplying tuples
+# list=vegetables*5
+# print(list)
+
+
+
