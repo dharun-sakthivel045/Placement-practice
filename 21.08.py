e7752e67d8dcfa3cfb371f06d0b13703dcb9c95d
Dharun S <dharunsakthivel720@gmail.com>
2025-08-21 19:26:19 +0530

    Add files via upload

---
+++ 21.08.py
@@ -0,0 +1,24 @@
+# a={'apple', 'banana', 'cherry'}
+# print(a)
+
+# a={'apple', 'banana', 'cherry','apple'}
+# print(a)  # Sets do not allow duplicate values, so 'apple' will only appear once.
+
+# a={'apple', 'banana', 'cherry',1,True,2,6}
+# print(a)  # This will print all unique elements, including integers and booleans.
+
+# a={'apple', 'banana', 'cherry',1,False,2,6,0}
+# print(a)  # This will print all unique elements, including integers and booleans.
+
+# a={'apple', 'banana', 'cherry',1,False,2,6,0}
+# print(len(a))  # This will print the number of unique elements in the set.
+
+# a={'apple', 'banana', 'cherry',1,False,2,6,0}
+# print(type(a))  # This will print the type of the variable, which is 'set'.
+
+a={'dahrun','nan','dharun'}
+b={1,3,5,67}
+c={True,False}
+print(a)
+print(b)
+print(c)
