1d84df2e56c4ad285e5f56d3501c901dfd17da9c
Dharun S <dharunsakthivel720@gmail.com>
2025-08-26 19:22:38 +0530

    Add files via upload

---
+++ 26.08.py
@@ -0,0 +1,39 @@
+# x={                             #dictionary
+#     "name":"dharun",
+#     "age":19,
+#     "location":"india"
+# }
+# print(x ["name"])
+
+# a={ "name":"dharun",   #dictionary
+#     "age":19,
+#     "location":"india"
+# }
+# print(a["name"])
+# print(a["age"])
+# print(a["location"])
+
+# a={"name":"dharun",   #in dictionary accessing the values
+#     "age":19,
+#     "location":"india"
+# }
+# x= a.get("name")
+# print(x)
+
+# a={"name":"dharun",
+#     "age":19,
+#     "location":"india"
+# }
+# x= a.keys()   #to get the keys of the dictionary
+# print(x)
+
+# a={"name":"dharun",
+#    "age":19,
+#    "location":"india"
+# }
+# x= a.keys()
+# print(x)
+# a["model"]="mustang"   #adding a new key-value pair to the dictionary
+# print(x)  #the keys view object will reflect the changes
+
+
